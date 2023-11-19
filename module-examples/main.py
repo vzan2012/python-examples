ddb0a6bca5d379b18c312bd12c4acdaa5d0e7864
Deepak Guptha <vzan2012@gmail.com>
2023-11-19 15:32:07 +0530

    feat: modules examples

---
+++ module-examples/main.py
@@ -0,0 +1,7 @@
+# from utility import addition
+import utility
+
+print(utility.addition(6, 5))
+print(utility.subtraction(6, 5))
+print(utility.divide(6, 5))
+print(utility.multiply(6, 5))
---
+++ module-examples/utility.py
@@ -0,0 +1,20 @@
+# Arithematic Operations
+
+# Addition
+def addition(num1, num2):
+    return num1+num2
+
+
+# Subtraction
+def subtraction(num1, num2):
+    return num1-num2
+
+
+# Division
+def divide(num1, num2):
+    return num1/num2
+
+
+# Multiply
+def multiply(num1, num2):
+    return num1*num2
